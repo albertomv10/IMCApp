26ab90389ea752d84b03987249abfbfa9c46c4ce
Alberto Medina <amedina10@alu.ucam.edu>
2024-07-14 13:22:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.imcapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
+
+class MainActivity : AppCompatActivity() {
+    private var isMaleSelected = true
+    private var isFemaleSelected = false
+    private var currentHeight = 130
+    private var currentWeight: Int = 70
+    private var currentAge: Int = 25
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+    private lateinit var txtHeight: TextView
+    private lateinit var range: RangeSlider
+    private lateinit var btnSumWeight: FloatingActionButton
+    private lateinit var btnResWeight: FloatingActionButton
+    private lateinit var txtWeight: TextView
+    private lateinit var btnSumAge: FloatingActionButton
+    private lateinit var btnResAge: FloatingActionButton
+    private lateinit var txtAge: TextView
+    private lateinit var btnCalculate: Button
+
+    companion object{
+        const val IMC_KEY = "IMC_Result"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initComponents()
+        initListeners()
+        initUi()
+    }
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.cardView1)
+        viewFemale = findViewById(R.id.cardView2)
+        txtHeight = findViewById(R.id.height)
+        range = findViewById(R.id.range)
+        btnSumWeight = findViewById(R.id.btnSumWeight)
+        btnResWeight = findViewById(R.id.btnResWeight)
+        txtWeight = findViewById(R.id.textWeight)
+        btnSumAge = findViewById(R.id.btnSumAge)
+        btnResAge = findViewById(R.id.btnResAge)
+        txtAge = findViewById(R.id.textAge)
+        btnCalculate = findViewById(R.id.button)
+    }
+    private fun initUi() {
+        updateButtonColors()
+        txtWeight.text = currentWeight.toString()
+        txtAge.text = currentAge.toString()
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener { selectGender(isMale = true) }
+        viewFemale.setOnClickListener { selectGender(isMale = false) }
+        range.addOnChangeListener { _, value, _ ->
+            currentHeight = value.toInt()
+            val txtAltura = "$currentHeight cm"
+            txtHeight.text = txtAltura
+        }
+        btnSumWeight.setOnClickListener {
+            currentWeight++
+            txtWeight.text = currentWeight.toString()
+        }
+        btnResWeight.setOnClickListener {
+            if (currentWeight>0) {
+                currentWeight--
+                txtWeight.text = currentWeight.toString()
+            }
+        }
+        btnSumAge.setOnClickListener {
+            currentAge++
+            txtAge.text = currentAge.toString()
+        }
+        btnResAge.setOnClickListener {
+            if (currentAge>0) {
+                currentAge--
+                txtAge.text = currentAge.toString()
+            }
+        }
+        btnCalculate.setOnClickListener {
+            val result = calculateIMC()
+            navigateToResult(result)
+        }
+    }
+
+    private fun navigateToResult(result: Double) {
+        val intent = Intent(this, ResultIMCActivity::class.java)
+        intent.putExtra("IMC_Result", result)
+        startActivity(intent)
+    }
+
+    private fun selectGender(isMale: Boolean) {
+        isMaleSelected = isMale
+        isFemaleSelected = !isMale
+        updateButtonColors()
+    }
+
+    private fun updateButtonColors() {
+        viewMale.setCardBackgroundColor(getCardColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getCardColor(isFemaleSelected))
+    }
+
+    private fun getCardColor(isSelected: Boolean): Int {
+        val colorReference = if (isSelected) {
+            R.color.selected_button
+        } else {
+            R.color.button
+        }
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun calculateIMC():Double{
+        return currentWeight/(currentHeight*currentHeight*0.0001)
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/imcapp/ResultIMCActivity.kt
@@ -0,0 +1,84 @@
+package com.example.imcapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.imcapp.MainActivity.Companion.IMC_KEY
+import java.text.DecimalFormat
+
+class ResultIMCActivity : AppCompatActivity() {
+    lateinit var txtIMC:TextView
+    lateinit var txtIndicator: TextView
+    lateinit var txtExplain: TextView
+    lateinit var image:ImageView
+    lateinit var btnBack: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_imcactivity)
+        initComponents()
+        initiListeners()
+    }
+
+    private fun updateResult(imc:Double) {
+        val decimalFormat = DecimalFormat("#.00")
+        val result = decimalFormat.format(imc)
+        txtIMC.text = result
+        calculateHealth(imc)
+    }
+
+    private fun initComponents() {
+        txtIMC = findViewById(R.id.imcText)
+        txtIndicator = findViewById(R.id.indicatorText)
+        txtExplain = findViewById(R.id.explainText)
+        image = findViewById(R.id.image)
+        btnBack = findViewById(R.id.buttonVolver)
+        val imc = intent.extras?.getDouble(IMC_KEY) ?: 0.0
+        updateResult(imc)
+    }
+
+    private fun initiListeners(){
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun calculateHealth(result:Double){
+        when(result){
+            in 0.0..18.49 ->{
+                txtIndicator.text = "BAJO PESO"
+                txtIndicator.setTextColor(ContextCompat.getColor(this, R.color.peso_bajo))
+                txtExplain.text = "Estas en los huesos, pide unos cuantos knoweats que están muy buenos"
+                image.setImageResource(R.drawable.xokasesqueleto)
+            }
+            in 18.50..24.99 -> {
+                txtIndicator.text = "PESO NORMAL"
+                txtIndicator.setTextColor(ContextCompat.getColor(this, R.color.peso_normal))
+                txtExplain.text = "Buen peso bro pero no te confies, shhh y te callas."
+
+                image.setImageResource(R.drawable.xokasnormal)
+            }
+            in 25.00..29.99 ->{
+                txtIndicator.text = "SOBREPESO"
+                txtIndicator.setTextColor(ContextCompat.getColor(this, R.color.sobrepeso))
+                txtExplain.text = "Estas modo El Fokas, comprate una casa de 2 millones de euros con un gimnasio privado y entrenador personal"
+                image.setImageResource(R.drawable.xokassobrepeso2)
+            }
+            in 30.00..100.00 -> {
+                txtIndicator.text = "OBESIDAD"
+                txtIndicator.setTextColor(ContextCompat.getColor(this, R.color.obesidad))
+                txtExplain.text = "Estás gordito, ¿y que?"
+                image.setImageResource(R.drawable.xokasobesidad)
+            }
+        }
+
+    }
+}
